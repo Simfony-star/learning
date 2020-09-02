ec9d07d47684ff0c061ed64ff0011e4571719cb5
Simfony <endorfine902@gmail.com>
2020-09-02 19:13:49 +0300

    Task complete

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@
 
 let i = true;
 
+let personalMovieDB = {
+
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
 while (i) {
 
     const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', 'Число');
@@ -9,7 +19,30 @@ while (i) {
     if (isNaN(numberOfFilms)) {
         alert('Только числа');
     } else {
+        personalMovieDB.count = numberOfFilms;
         i = false;
-        document.querySelector('.inst').innerHTML = `Количество просмотренных вами фильмов: ${numberOfFilms}`;
+        
+        document.querySelector('.inst').innerHTML = `Количество просмотренных вами фильмов: ${personalMovieDB.count}`;
     }
 }
+
+i = true;
+
+while (i) {
+    const lastMovie = prompt('Какой последний фильм вы посмотрели ?', 'Название фильма');
+    const userRaitingMovie = prompt('Оцените этот фильм от 1 до 10', '1-10');
+
+
+    if (isNaN(userRaitingMovie)) {
+        alert('В оценке только числа');
+    } else {
+        i = false;
+        personalMovieDB.movies = {
+            name: lastMovie,
+            raiting: userRaitingMovie
+        };
+
+    }
+}
+
+console.log(personalMovieDB);
